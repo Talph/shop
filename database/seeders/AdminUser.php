d10a04c0fec355e64e378efe075dc7abe2cafeda
Tafara Shamu <talph@outlook.com>
2022-03-09 21:03:22 +0200

    Database seeders user/products

---
+++ database/seeders/AdminUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class AdminUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $adminRole = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
+            'remember_token' => Str::random(10)
+        ]);
+    }
+}
