8e6aa64338bc83991a013d780aa865bb47865a84
Tafara Shamu <talph@outlook.com>
2022-03-11 17:31:11 +0200

    logout form when on the shop page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,9 +56,12 @@
                             <li>
                                 <a class="nav-link" href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
-                                                                                             document.getElementById('logout-form').submit();">
+                                         document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
                                 </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
                             </li>
                             @endguest
                         </ul>
